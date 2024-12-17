a3d1d7831f58b5bf77c7ace6585156319b008a82
Karolis Lipinskas <karolis.lipi@gmail.com>
2024-12-17 15:52:09 +0200

    Removed magic values and hard coded UI references; Fixed long method loadData() by breaking it into smaller ones; Removed Code Duplication for navigating to other pages.

---
+++ Lab1/src/main/java/model/Constants.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Constants {
+    // Cart stuff
+    public static final String STATUS_OPEN = "open";
+    public static final String STATUS_PAID = "Paid";
+    public static final String ZERO_PRICE = "0.0";
+
+    // Navigation
+    public static final String MAIN_PAGE = "Main page";
+    public static final String SETTINGS_PAGE = "Settings page";
+    public static final String ORDER_HISTORY_PAGE = "Order history page";
+    public static final String LOGIN_PAGE = "Login screen";
+
+    public static final String MAIN_PAGE_PATH = "../fxml/main.fxml";
+    public static final String SETTINGS_PAGE_PATH = "../fxml/settings.fxml";
+    public static final String ORDER_HISTORY_PAGE_PATH = "../fxml/orderList.fxml";
+    public static final String LOGIN_PAGE_PATH = "../fxml/login.fxml";
+
+    // Table column names
+    public static final String NAME_COLUMN = "name";
+    public static final String TYPE_COLUMN = "type";
+    public static final String QUANTITY_COLUMN = "quantity";
+    public static final String WARRANTY_COLUMN = "warranty";
+    public static final String PRICE_COLUMN = "price";
+
+    // Persistence stuff
+    public static final String PERSISTENCE_UNIT_NAME = "kl_kursinis";
+}
